6fa994d14bdc02f6aa38f4c78c3951badc5a9918
Stefan Inzer <stefan.inzer@gmail.com>
2025-07-21 17:00:40 -0700

    visualization

--- visualization.py
+++ visualization.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set_theme()
 
 
 data = pd.read_csv("fft_hpfilter/season_trend_decomp.csv", header=0)
@@ -11,5 +12,12 @@ data['Quarter'] = pd.PeriodIndex(data['Quarter'], freq='Q').to_timestamp()
 
 datamelt = data.melt('Quarter', var_name='cols', value_name='vals')
 
-sns.catplot(x="Quarter", y="vals", hue='cols', data=datamelt, kind='line')
+fig,axs = plt.subplots(nrows=3)
+fig.suptitle("Season Trend Decomposition of Quarterly Australian Electricity Production")
+
+sns.lineplot(data, ax=axs[0], x="Quarter", y = "Original")
+sns.lineplot(data, ax=axs[1], x="Quarter", y = "Trend")
+sns.lineplot(data, ax=axs[2], x="Quarter", y = "Seasonal")
 plt.show()
+
+plt.savefig("Season-Trend Decomposition",format="svg")
